c69ba71b57fae4f401a8b966d203f8d9fb3e0ce4
esraltintas <es.altintas@gmail.com>
2013-10-03 23:34:17 +0300

    Dosyayi okuyup indexi arttiran veri uretildi

--- rdg.rb
+++ rdg.rb
@@ -1,7 +1,13 @@
+index = 1
 file = File.open("/home/esra/VeriUretec/dosya.txt", "r+")
-while (line = file.gets)
+#file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
+line = file.gets
+while (index<10)
+      file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
       puts " #{line}"
-  end
-  file.close
+      index = index +1
+end
+file.close
+
   
 
