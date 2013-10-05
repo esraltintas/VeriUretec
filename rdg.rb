ffeefb93ba8ce95f2a387e722680e94667b125d5
esraltintas <es.altintas@gmail.com>
2013-10-05 11:35:05 +0300

    Veri tiplerinin alanlarindan kacar tane oldugu dosyadan okundu oradaki index arttirilarak henuz ayni veriyi yazdiran bir dosya olusturuldu

--- rdg.rb
+++ rdg.rb
@@ -1,13 +1,18 @@
-index = 1
-file = File.open("/home/esra/VeriUretec/dosya.txt", "r+")
-#file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
-line = file.gets
-while (index<10)
-      file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
-      puts " #{line}"
-      index = index +1
+i =1
+array = IO.readlines("/home/esra/VeriUretec/dosya.txt")
+array2 = array[1]
+array3 = array2.split(" ")
+satirsayisi = Integer(array3[0])
+kacint = Integer(array3[1])
+kacchar = Integer(array3[2])
+kactarih = Integer(array3[3])
+kacmail = Integer(array3[4])
+gdata = File.new("/home/esra/VeriUretec/data.txt", "w")
+
+while (i<=satirsayisi)
+	gdata.puts("#{i} ads 121")
+      #file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
+ #     puts " #{line}"
+	i = i +1
 end
-file.close
-
-  
-
+gdata.close
