771bd0fcc1460a87c57f01d0a3b9416af5183989
esraltintas <es.altintas@gmail.com>
2013-10-05 17:22:57 +0300

    Uretilmek istenen integer sayisi kadar integer bir dizi seklinde üretildi

--- rdg.rb
+++ rdg.rb
@@ -1,4 +1,5 @@
 i =1
+int = []
 array = IO.readlines("/home/esra/VeriUretec/dosya.txt")
 array2 = array[1]
 array3 = array2.split(" ")
@@ -10,7 +11,12 @@ kacmail = Integer(array3[4])
 gdata = File.new("/home/esra/VeriUretec/data.txt", "w")
 
 while (i<=satirsayisi)
-	gdata.puts("#{i} ads 121")
+	j=1
+	while (j <= kacint)
+		int[j-1] = rand(9)
+		j = j+1
+	end
+	gdata.puts("#{i} #{int} 121")
       #file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
  #     puts " #{line}"
 	i = i +1
