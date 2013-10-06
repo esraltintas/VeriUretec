902bb290b0e1fee68aebf86993b6398f8fafabc9
esraltintas <es.altintas@gmail.com>
2013-10-06 06:17:41 +0300

    Istenilen sayida rasgele karakter uretildi

--- rdg.rb
+++ rdg.rb
@@ -10,15 +10,30 @@ kactarih = Integer(array3[3])
 kacmail = Integer(array3[4])
 gdata = File.new("/home/esra/VeriUretec/data.txt", "w")
 
+def self.random(length=kacchar)
+	    (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).sort_by {rand}[0,length].join # a dan zye dosyada kac karakter istiyorsa o kadar karakter secer
+end
 while (i<=satirsayisi)
 	j=1
 	while (j <= kacint)
 		int[j-1] = rand(9)
 		j = j+1
 	end
-	gdata.puts("#{i} #{int} 121")
-      #file.puts "#{index} 42 esra 21.03.2013 es.altintas@gmail.com"
- #     puts " #{line}"
+	j=1
+	while (j <= kacchar)
+                char = self.random(kacchar)
+		j = j+1
+        end
+#	j=1
+#	while (j <= kactarih)
+ #               int[j-1] = rand(9)
+#		j=j+1
+ #       end
+#	j=1
+#	while (j <= kacchar)
+ #               int[j-1] = rand(9)
+  #      end          
+	gdata.puts("#{i} #{int} #{char} 121")
 	i = i +1
 end
 gdata.close
