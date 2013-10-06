833c8aa7dd788d347c8f76bfee201e7036c385dd
esraltintas <es.altintas@gmail.com>
2013-10-06 07:30:47 +0300

    Dosyadan okudugu sayida satir olusturuldu, bu satirlarda istenildigi kadar rasgele integer, char, date ve mail olusturuldu

--- rdg.rb
+++ rdg.rb
@@ -1,5 +1,8 @@
 i =1
 int = []
+date = []
+char = []
+mail = []
 array = IO.readlines("/home/esra/VeriUretec/dosya.txt")
 array2 = array[1]
 array3 = array2.split(" ")
@@ -10,8 +13,16 @@ kactarih = Integer(array3[3])
 kacmail = Integer(array3[4])
 gdata = File.new("/home/esra/VeriUretec/data.txt", "w")
 
-def self.random(length=kacchar)
-	    (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).sort_by {rand}[0,length].join # a dan zye dosyada kac karakter istiyorsa o kadar karakter secer
+def self.randomchar(length = kacchar)
+	    (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).sort_by {rand}[0,1].join # a dan zye dosyada kac karakter istiyorsa o kadar karakter secer
+end
+
+def self.randomdate(length = kactarih)
+	((('1'..'30').to_a).sort_by {rand}[0,1].join + "." + (('1'..'12').to_a).sort_by {rand}[0,1].join + "." + (('1990'..'2999').to_a).sort_by {rand}[0,1].join)  
+end
+
+def self.randommail(length = kacmail)
+	((0...4).map{ ('a'..'z').to_a[rand(29)] }.join + "@mail.com")
 end
 while (i<=satirsayisi)
 	j=1
@@ -19,21 +30,25 @@ while (i<=satirsayisi)
 		int[j-1] = rand(9)
 		j = j+1
 	end
+	
 	j=1
 	while (j <= kacchar)
-                char = self.random(kacchar)
+                char[j-1] = self.randomchar(kacchar)
 		j = j+1
         end
-#	j=1
-#	while (j <= kactarih)
- #               int[j-1] = rand(9)
-#		j=j+1
- #       end
-#	j=1
-#	while (j <= kacchar)
- #               int[j-1] = rand(9)
-  #      end          
-	gdata.puts("#{i} #{int} #{char} 121")
+	
+	j=1
+	while (j <= kactarih)
+                date[j-1] = self.randomdate(kactarih)
+		j=j+1
+        end
+	
+	j=1
+	while (j <= kacmail)
+                mail[j-1] = self.randommail(kacmail)
+		j = j+1
+        end          
+	gdata.puts("#{i} #{int * ","} #{char * ","} #{date * ","} #{mail * ","}")
 	i = i +1
 end
 gdata.close
