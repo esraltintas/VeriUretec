04a5836b0c5f04d7a06964a985703371f33c848e
esraltintas <es.altintas@gmail.com>
2013-10-06 07:38:02 +0300

    Dosyadan okudugu sayida satir olusturuldu, bu satirlarda istenildigi kadar rasgele integer, char, date ve mail olusturuldu

--- rdg.rb
+++ rdg.rb
@@ -3,15 +3,15 @@ int = []
 date = []
 char = []
 mail = []
-array = IO.readlines("/home/esra/VeriUretec/dosya.txt")
+array = IO.readlines("/home/esra/VeriUretec/dosya.txt") #Burada dosya okunuyor ve bu bir diziye ataniyor
 array2 = array[1]
-array3 = array2.split(" ")
-satirsayisi = Integer(array3[0])
+array3 = array2.split(" ") # Bosluk karakterini çıkartıp bunu baska bir diziye atadim
+satirsayisi = Integer(array3[0]) # Dizide elemanlar karakter oldugu için once sayi haline getirdim
 kacint = Integer(array3[1])
 kacchar = Integer(array3[2])
 kactarih = Integer(array3[3])
 kacmail = Integer(array3[4])
-gdata = File.new("/home/esra/VeriUretec/data.txt", "w")
+gdata = File.new("/home/esra/VeriUretec/data.txt", "w") #Yeni bir dosya olusturdum ve buna yazma izni verdim
 
 def self.randomchar(length = kacchar)
 	    (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).sort_by {rand}[0,1].join # a dan zye dosyada kac karakter istiyorsa o kadar karakter secer
@@ -48,7 +48,7 @@ while (i<=satirsayisi)
                 mail[j-1] = self.randommail(kacmail)
 		j = j+1
         end          
-	gdata.puts("#{i} #{int * ","} #{char * ","} #{date * ","} #{mail * ","}")
+	gdata.puts("#{i} #{int * ","} #{char * ","} #{date * ","} #{mail * ","}") # Rasgele olusturulmus degerleri olusturdugum dosya icine yazdirdim
 	i = i +1
 end
 gdata.close
