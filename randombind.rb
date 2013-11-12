4b4cb58052670245ce658018664af0cdabba2735
esraltintas <es.altintas@gmail.com>
2013-11-12 21:09:35 +0200

    A kaydı için random log olusturuldu

--- randombind.rb
+++ randombind.rb
@@ -1,36 +1,38 @@
-class BIND 
-	def mail
-		tld  = [".com", ".org", ".net", ".us", ".tl", ".tr"]
-                host = ["@mail", "@gmail", "@qmail", "@hotmail", "@kmail", "@ipsum"]
-                mail = ((0...4).map{ ('a'..'z').to_a[rand(29)] }.join + (host.to_a).sort_by {rand}[0,1].join + (tld.to_a).sort_by {rand}[0,1].join)
+$time = Time.now
+$port = (('1000'..'65536').to_a).sort_by {rand}[0,1].join
+$ip = ((('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join)
 
+class BIND
+	
+	def URL
+                tld  = [".com", ".org", ".net", ".us", ".tl", ".tr"]
+
+                url = ((('0'..'10').to_a).sort_by {rand}[0,1].join + "." + (0...8).map{ ('a'..'z').to_a[rand(29)] }.join  + (tld.to_a).sort_by {rand}[0,1].join)
 	end
+
+
 	
 	def arecord
-		ip = ((('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join)
-		return "mail IN A " +ip
+		return  "#$time queries: info: client #$ip #  #$port  :  \ " + "\n  query:" +  URL() + " IN A -"
 	end
 
-	def cname
-		return "mail IN CNAME www\n" + arecord()
+	#def cname
+	#	return "mail IN CNAME www\n" + arecord()
 		
-	end
+	#end
 
-	def mx
-		return "IN MX 10 mail." + mail()
-	end
+	#def mx
+	#	return "IN MX 10 mail." + mail()
+	#end
 	
-	def nameserver
-		return "IN NS ns." + mail() + "\nns " + arecord()	
-	end
+	#def nameserver
+	#	return "IN NS ns." + mail() + "\nns " + arecord()	
+	#end
 		
 end
 
 bind =BIND.new
 puts bind.arecord()
-puts bind.cname()
-puts bind.mail()
-puts bind.nameserver()
 
 
 
