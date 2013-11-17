6c4d27dc728438896e46a03f4281d097a7e083ea
esraltintas <es.altintas@gmail.com>
2013-11-17 11:31:38 +0200

    Cname, a, mx ve ns kaydı için bind logu üretildi

--- randombind.rb
+++ randombind.rb
@@ -1,11 +1,22 @@
-$date = ((('2000'..'2013').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'12').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'28').to_a).sort_by {rand}[0,1].join)
-$time = ((('00'..'23').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join)
-$ms = (('100'..'1000').to_a).sort_by {rand}[0,1].join
-$port = (('1000'..'65536').to_a).sort_by {rand}[0,1].join
-$ip = ((('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join)
 pv=[10,20] #preference-value
 $randpv = pv[Random.rand(0..1)]
 class BIND
+	def port
+		port = (('1000'..'65536').to_a).sort_by {rand}[0,1].join
+	end
+	
+	def time
+		date = ((('2000'..'2013').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'12').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'28').to_a).sort_by {rand}[0,1].join)
+		time = ((('00'..'23').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join)
+		ms = (('100'..'1000').to_a).sort_by {rand}[0,1].join
+
+		time2 = date + " " + time + "." + ms
+	end	
+
+	def ip
+		ip = ((('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join + "." + (('0'..'250').to_a).sort_by {rand}[0,1].join)
+
+	end
 	
 	def URL
                 tld  = [".com", ".org", ".net", ".us", ".tl", ".tr"]
@@ -16,21 +27,21 @@ class BIND
 
 	
 	def arecord
-		return  "#$date #$time.#$ms queries: info: client #$ip #  #$port  :  \ " + "\n  query:" +  URL() + " IN A #$ip"
+		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  URL() + " IN A #$ip"
 	end
 
 	def cname
-		return "#$date #$time.#$ms queries: info: client #$ip #  #$port  :  \ " + "\n  query:"+ URL() + "mail IN CNAME " + URL()
+		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + URL() + "mail IN CNAME " + URL()
 		
 	end
 
 	def mx
 		url = URL()
-		return "#$date #$time.#$ms queries: info: client #$ip #  #$port  :  \ " + "\n  query:" +  url + " IN MX " + "#$randpv " + url
+		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  url + " IN MX " + "#$randpv " + url
 	end
 	
 	def nameserver
-		return "#$date #$time.#$ms queries: info: client #$ip #  #$port  :  \ " + "\n  query:"+"IN NS " + URL()
+		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + "IN NS " + URL()
 	end
 		
 end
