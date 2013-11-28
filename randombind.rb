3942398de46d055d8580d7c76c5edbaed44c6d40
esraltintas <es.altintas@gmail.com>
2013-11-29 01:11:53 +0200

    Tarih uretme sorunu cozulmeye calisildi

--- randombind.rb
+++ randombind.rb
@@ -1,16 +1,17 @@
 pv=[10,20] #preference-value
+def time_rand from = 0.0, to = Time.now
+  Time.at(from + rand * (to.to_f - from.to_f))
+end
 $randpv = pv[Random.rand(0..1)]
+$rand_date = time_rand Time.local(2010, 7, 1), Time.now
+
 class BIND
 	def port
 		port = (('1000'..'65536').to_a).sort_by {rand}[0,1].join
 	end
-	
-	def time
-		date = ((('2000'..'2013').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'12').to_a).sort_by {rand}[0,1].join + "-" + (('01'..'28').to_a).sort_by {rand}[0,1].join)
-		time = ((('00'..'23').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join + ":" + (('00'..'59').to_a).sort_by {rand}[0,1].join)
-		ms = (('100'..'1000').to_a).sort_by {rand}[0,1].join
 
-		time2 = date + " " + time + "." + ms
+	def time
+		next_date = time_rand Time.local(2010, 7, 1), $rand_date
 	end	
 
 	def ip
@@ -27,21 +28,23 @@ class BIND
 
 	
 	def arecord
-		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  URL() + " IN A #$ip"
+		return   time().to_s + "queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  URL() + " IN A #$ip"
 	end
 
 	def cname
-		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + URL() + "mail IN CNAME " + URL()
+
+		return  time().to_s + "queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + URL() + "mail IN CNAME " + URL()
 		
 	end
 
 	def mx
 		url = URL()
-		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  url + " IN MX " + "#$randpv " + url
+		return  time().to_s + "queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" +  url + " IN MX " + "#$randpv " + url
 	end
 	
 	def nameserver
-		return  time() + " queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + "IN NS " + URL()
+
+		return  time().to_s + "queries: info: client " + ip() + " #  " + port() + "  :  \  \n" + "  query:" + "IN NS " + URL()
 	end
 		
 end
