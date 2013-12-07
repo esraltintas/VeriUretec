e10d86fab0431f93e17bdac8b1cef64e469e9508
esraltintas <es.altintas@gmail.com>
2013-12-08 00:53:01 +0200

    URL degistirildi

--- function.rb
+++ function.rb
@@ -27,7 +27,7 @@ class Function
 	end
 
 	def URL
-		tld  = [".com", ".org", ".net", ".us", ".tl", "com.tr", ".biz", ".pw", ".info", ".gen.tr", ".web.tr", " .co", ".eu"]
+		tld = [".com",".net", ".biz", ".biz", ".org", ".info", ".us", "co.uk", ".de", ".co", ".io", ".me", ".pw", ".ca", ".tv", ".in", ".mobi", ".es", ".com.es", ".nom.es", ".org.es", ".com.au", ".net.au", ".org.au", ".eu", ".cc", ".org.uk", ".fr", ".pe", ".us.com", ".asia", ".ch", ".bz", "me.uk", "ws", ".xxx", ".sg", ".com.sg", ".li", ".de.com", ".cm", ".com.pe", ".net.pe", ".org.pe", ".nu", ".com.tr", ".gen.tr", ".web.tr", ".name.tr", ".tv.tr", "net.tr", ".gov.tr", ".bel.tr", ".edu.tr", ".org.tr", ".bbs.tr", ".av.tr", ".dr.tr", ".gs" ]
 
 		url = ((('0'..'10').to_a).sort_by {rand}[0,1].join + "." + (0...8).map{ ('a'..'z').to_a[rand(29)] }.join  + (tld.to_a).sort_by {rand}[0,1].join)
 	end
